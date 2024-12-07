57c2de66557d7a0b803173a0281f3ab2489dde4c
Geraldus Citer Teja <duate@gmail.com>
2024-12-07 11:50:03 +0700

    update deploy 2

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
 
-    <BrowserRouter basename="/EdgeProperty">
+    <BrowserRouter basename="/EdgeProperty/">
       <App />
     </BrowserRouter>
 );
